efd7b900ed94dcfe2d7e8523862d25dac8732d63
William Fitzjohn <wfitzjoh@uoguelph.ca>
2022-11-16 13:59:34 -0500

    Got docker container running flask in production

--- dash/wsgi.py
+++ dash/wsgi.py
@@ -3,6 +3,8 @@ from os import environ as env
 
 app = init_app()
 
+def returnApp():
+    return app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=env.get('PORT', 8051) , debug=env.get('DEBUG', 'false').lower() in ('true', '1', 't'))
